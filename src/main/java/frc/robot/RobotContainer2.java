407ee101b925ee5714ab93706a147b2602af89bf
Devesh Kotak <kdevu1997@Gmail.com>
2024-04-08 11:58:24 -0400

    Adding Drive Commands, in progress

---
+++ src/main/java/frc/robot/RobotContainer2.java
@@ -0,0 +1,72 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.Constants.OIConstants;
+import frc.robot.subsystems.SwerveSubsystem;
+import frc.utils.devices.BeamBreak.Phase;
+
+import java.io.File;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a "declarative" paradigm, very
+ * little robot logic should actually be handled in the {@link Robot} periodic methods (other than the scheduler calls).
+ * Instead, the structure of the robot (including subsystems, commands, and trigger mappings) should be declared here.
+ */
+public class RobotContainer2{
+
+    // The robot's subsystems and commands are defined here...
+    final CommandXboxController driverXbox = new CommandXboxController(0);
+
+    // File Directory With Swerve JSON Files (src/main/deploy/swerve)
+    File swerveJsonDirectory = new File(Filesystem.getDeployDirectory(),"swerve");
+
+    // Creating Swerve Drive 
+    SwerveSubsystem m_swerveDrive = new SwerveSubsystem(swerveJsonDirectory);
+    
+    private void configureDriverXbox(boolean isRedAlliance) {
+    
+    // Brake command (Left Trigger)
+    this.driverXbox.leftTrigger().whileTrue(new RunCommand(() -> m_swerveDrive.setX(),m_swerveDrive));
+   
+    final double invert = isRedAlliance ? -1 : 1;
+
+    m_swerveDrive.setDefaultCommand(
+                // The left stick controls translation of the robot.
+                // Turning is controlled by the X axis of the right stick.
+                new RunCommand(
+                        () -> m_swerveDrive.driveCommand(
+                                -MathUtil.applyDeadband(driverXbox.getLeftY(), OIConstants.kDriveDeadband) * invert,
+                                -MathUtil.applyDeadband(driverXbox.getLeftX(), OIConstants.kDriveDeadband) * invert,
+                                -MathUtil.applyDeadband(driverXbox.getRightX(), OIConstants.kTurnDeadband)),
+                        m_swerveDrive));
+
+    }
+
+     /**
+   * Sets the wheels into an X formation to prevent movement.
+   * This does not set the brake mode of the motors.
+   */
+  public void setX() {
+    m_frontLeft.setDesiredState(new SwerveModuleState(0, Rotation2d.fromDegrees(45)));
+    m_frontRight.setDesiredState(new SwerveModuleState(0, Rotation2d.fromDegrees(-45)));
+    m_rearLeft.setDesiredState(new SwerveModuleState(0, Rotation2d.fromDegrees(-45)));
+    m_rearRight.setDesiredState(new SwerveModuleState(0, Rotation2d.fromDegrees(45)));
+  }
+
+}
---
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -0,0 +1,73 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import java.io.File;
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.wpilibj.Filesystem;
+import swervelib.parser.SwerveControllerConfiguration;
+import swervelib.parser.SwerveDriveConfiguration;
+import swervelib.parser.SwerveParser;
+import swervelib.SwerveDrive;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.util.Units;
+import swervelib.telemetry.SwerveDriveTelemetry;
+import swervelib.telemetry.SwerveDriveTelemetry.TelemetryVerbosity;
+
+
+public class SwerveSubsystem extends SubsystemBase {
+
+// Intializing Swerve Object
+SwerveDrive swerveDrive;
+
+// Max Speed
+double maximumSpeed = Units.feetToMeters(4.5);
+
+
+
+    public SwerveSubsystem(File m_directory){
+          
+        // Configure the Telemetry before creating the SwerveDrive to avoid unnecessary objects being created.
+        SwerveDriveTelemetry.verbosity = TelemetryVerbosity.HIGH;
+
+        try{
+          swerveDrive = new SwerveParser(m_directory).createSwerveDrive(maximumSpeed);
+
+        } catch (Exception error){
+          throw new RuntimeException(error);
+        }
+        swerveDrive.setHeadingCorrection(false); // Heading correction should only be used while controlling the robot via angle.
+        swerveDrive.setCosineCompensator(!SwerveDriveTelemetry.isSimulation); // Disables cosine compensation for simulations since it causes discrepancies not seen in real life.
+        //setupPathPlanner();
+      }
+
+
+      /**
+   * Command to drive the robot using translative values and heading as angular velocity.
+   *
+   * @param translationX     Translation in the X direction.
+   * @param translationY     Translation in the Y direction.
+   * @param angularRotationX Rotation of the robot to set
+   * @return Drive command.
+   */
+  public Command driveCommand(double translationX, double translationY, double angularRotationX)
+  {
+    return run(() -> {
+      // Make the robot move
+      swerveDrive.drive(new Translation2d(translationX * swerveDrive.getMaximumVelocity(),
+                                          translationY * swerveDrive.getMaximumVelocity()),
+                        angularRotationX * swerveDrive.getMaximumAngularVelocity(),
+                        false,
+                        false);
+    });
+  }
+
+    
+  
+
+
+
+}
+
+
